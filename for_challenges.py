97297c7ad5556a207c3e70f13302b60838b2506f
Evgenii Nosov <dekardy@mail.ru>
2023-05-25 18:52:16 +0200

    Сделал задачи 1-4 из for_challenges. Есть вопрос к задаче 5

--- for_challenges.py
+++ for_challenges.py
@@ -2,7 +2,8 @@
 # Необходимо вывести имена всех учеников из списка с новой строки
 
 names = ['Оля', 'Петя', 'Вася', 'Маша']
-# ???
+for i in names: 
+    print(i, end='\n')
 
 
 # Задание 2
@@ -12,7 +13,8 @@ names = ['Оля', 'Петя', 'Вася', 'Маша']
 # Петя: 4
 
 names = ['Оля', 'Петя', 'Вася', 'Маша']
-# ???
+for i in names:
+    print(f'{i}: {len(i)}', end='\n')
 
 
 # Задание 3
@@ -25,7 +27,8 @@ is_male = {
     'Маша': False,
 }
 names = ['Оля', 'Петя', 'Вася', 'Маша']
-# ???
+for i in is_male:
+    print(f'{i}: {is_male[i]}')
 
 
 # Задание 4
@@ -40,8 +43,26 @@ groups = [
     ['Вася', 'Маша', 'Саша', 'Женя'],
     ['Оля', 'Петя', 'Гриша'],
 ]
-# ???
 
+
+len_dict = len(groups)
+group_numbers=[]
+
+t_p = len_dict
+
+while t_p > 0 :
+    group_numbers.append(t_p)
+    t_p=t_p-1
+
+group_numbers.reverse()
+#print(new_dict)
+
+general_dict=dict(zip(group_numbers,groups))
+#print(general_dict)
+
+print(f'Всего {len_dict} группы.', end='\n')
+for i in group_numbers:
+    print(f'Группа {i}: {len(general_dict[i])} ученика')
 
 # Задание 5
 # Для каждой пары учеников нужно с новой строки перечислить учеников, которые в неё входят
@@ -54,4 +75,26 @@ groups = [
     ['Оля', 'Петя', 'Гриша'],
     ['Вася', 'Маша', 'Саша', 'Женя'],
 ]
-# ???
+
+len_dict = len(groups)
+group_numbers=[]
+
+t_p = len_dict
+
+while t_p > 0 :
+    group_numbers.append(t_p)
+    t_p=t_p-1
+
+group_numbers.reverse()
+#print(new_dict)
+
+general_dict=dict(zip(group_numbers,groups))
+#print(general_dict)
+
+#a=tuple(general_dict[1])
+#print(a)
+
+print(f'Всего {len_dict} группы.', end='\n')
+for i in group_numbers:
+    print(f'Группа {i}: {general_dict[i]} ')
+
